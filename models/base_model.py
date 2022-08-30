4cd216bdc98fe24f2cf04c6d47ea883973351836
Dani Uboa <uboadaniel@gmail.com>
2022-08-30 19:01:32 +0300

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -33,26 +33,29 @@ class BaseModel():
         models.storage.new(self)
 
     def __str__(self):
-        """ Returns a string representation of the object """
+        """
+        Returns the string representation
+        of the instance
+        """
         return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.__dict__
-        )
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """ Updates the public instance attribute
-        updated_at with current time
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """ Returns a dictionary containing all keys/values of __dict__
-        of the instance
         """
-        new_dict = dict(self.__dict__)
-        new_dict["created_at"] = self.created_at.isoformat(sep='T')
-        new_dict["updated_at"] = self.updated_at.isoformat(sep='T')
-        new_dict["__class__"] = self.__class__.__name__
-        return new_dict
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        dict = {**self.__dict__}
+        dict['__class__'] = type(self).__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
+
+        return dict
