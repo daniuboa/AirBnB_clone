71d10c7bfde98d65120060c66d1db6a71a8e5d78
Dani Uboa <uboadaniel@gmail.com>
2022-08-31 12:18:26 +0300

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel():
                 setattr(self, key, value)
             return
 
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
