2aabcfcc0ef9d9d973293e322835ac4ea3ccbfaa
Dani Uboa <uboadaniel@gmail.com>
2022-09-01 18:26:28 +0300

    update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,11 +7,11 @@ import os
 import json
 from models.base_model import BaseModel
 from models.user import User
-#from models.state import State
-#from models.city import City
-#from models.review import Review
-#from models.amenity import Amenity
-#from models.place import Place
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
 
 
 class FileStorage():
